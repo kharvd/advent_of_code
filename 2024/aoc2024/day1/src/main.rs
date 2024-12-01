d0f5134e1f28573b327cada038b5184b5a334859
Valery Kharitonov <val@kharvd.com>
2024-12-01 13:34:38 -0500

    day 1

--- 2024/aoc2024/day1/src/main.rs
+++ 2024/aoc2024/day1/src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+
+fn parse_line(s: &str) -> (u64, u64) {
+    let (a, b) = s.split_once("   ").unwrap();
+    (a.parse().unwrap(), b.parse().unwrap())
+}
+
+fn read_input() -> (Vec<u64>, Vec<u64>) {
+    let (vec1, vec2): (Vec<u64>, Vec<u64>) =
+        io::stdin().lines().map(|l| parse_line(&l.unwrap())).unzip();
+
+    (vec1, vec2)
+}
+
+fn part1() -> u64 {
+    let (mut vec1, mut vec2) = read_input();
+    vec1.sort();
+    vec2.sort();
+
+    vec1.iter().zip(vec2).map(|(x, y)| x.abs_diff(y)).sum()
+}
+
+fn part2() -> u64 {
+    let (vec1, vec2) = read_input();
+    vec1.iter()
+        .map(|x| (vec2.iter().filter(|y| *y == x).count() as u64) * x)
+        .sum()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let part = &std::env::args().collect::<Vec<_>>()[1];
+    if part == "1" {
+        println!("part1: {}", part1());
+    } else {
+        println!("part2: {}", part2());
+    }
 }
