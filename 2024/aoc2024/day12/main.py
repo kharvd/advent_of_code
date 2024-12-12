cbee6b135fa31581322c0a2414bd4c4d11eb0498
Valery Kharitonov <val@kharvd.com>
2024-12-12 00:43:08 -0500

    day 12 part 2

--- 2024/aoc2024/day12/main.py
+++ 2024/aoc2024/day12/main.py
@@ -48,6 +48,80 @@ def perimeter(field, region):
     return p
 
 
+def get(field, i, j):
+    if 0 <= i < len(field) and 0 <= j < len(field[i]):
+        return field[i][j]
+    return None
+
+
+def find_side(field, region, start, normal):
+    side = {(start, normal)}
+    i, j = start
+    ndi, ndj = normal
+    if ndi == 0:
+        di, dj = 1, 0
+    else:
+        di, dj = 0, 1
+
+    ii, jj = i + di, j + dj
+    while (
+        0 <= ii < len(field)
+        and 0 <= jj < len(field[0])
+        and (ii, jj) in region
+        and (
+            (
+                (get(field, i + ndi, j + ndj) is None)
+                and (get(field, ii + ndi, jj + ndj) is None)
+            )
+            or (
+                (get(field, i + ndi, j + ndj) != field[i][j])
+                and (get(field, ii + ndi, jj + ndj) != field[i][j])
+            )
+        )
+    ):
+        side.add(((ii, jj), normal))
+        ii, jj = ii + di, jj + dj
+
+    ii, jj = i - di, j - dj
+    while (
+        0 <= ii < len(field)
+        and 0 <= jj < len(field[0])
+        and (ii, jj) in region
+        and (
+            (
+                (get(field, i + ndi, j + ndj) is None)
+                and (get(field, ii + ndi, jj + ndj) is None)
+            )
+            or (
+                (get(field, i + ndi, j + ndj) != field[i][j])
+                and (get(field, ii + ndi, jj + ndj) != field[i][j])
+            )
+        )
+    ):
+        side.add(((ii, jj), normal))
+        ii, jj = ii - di, jj - dj
+    return side
+
+
+def num_sides(field, region):
+    excluded = set()
+    p = 0
+    for i, j in region:
+        for di, dj in deltas:
+            if ((i, j), (di, dj)) in excluded:
+                continue
+
+            if (
+                not (0 <= i + di < len(field) and 0 <= j + dj < len(field[0]))
+                or field[i][j] != field[i + di][j + dj]
+            ):
+                side = find_side(field, region, (i, j), (di, dj))
+                # print(side)
+                p += 1
+                excluded = excluded | side
+    return p
+
+
 def part1():
     field = read_input()
     visited = set()
@@ -57,6 +131,20 @@ def part1():
             if (i, j) not in visited:
                 region = bfs(field, (i, j))
                 total += len(region) * perimeter(field, region)
+                visited = visited | region
+    print(total)
+
+
+def part2():
+    field = read_input()
+    visited = set()
+    total = 0
+    for i in range(len(field)):
+        for j in range(len(field[0])):
+            if (i, j) not in visited:
+                region = bfs(field, (i, j))
+                # print(field[i][j], num_sides(field, region))
+                total += len(region) * num_sides(field, region)
                 visited = visited | region
     print(total)
 
