9293999ad37c7c44179aad920c9f82156e11ff7b
Valery Kharitonov <val@kharvd.com>
2024-12-15 14:29:15 -0500

    day 15 part 2

--- 2024/aoc2024/day15/main.py
+++ 2024/aoc2024/day15/main.py
@@ -12,6 +12,27 @@ def read_input():
     return field, moves
 
 
+def blowup(field):
+    new_field = []
+    for line in field:
+        new_line = []
+        for c in line:
+            if c == "#":
+                new_line.append("#")
+                new_line.append("#")
+            elif c == "O":
+                new_line.append("[")
+                new_line.append("]")
+            elif c == ".":
+                new_line.append(".")
+                new_line.append(".")
+            elif c == "@":
+                new_line.append("@")
+                new_line.append(".")
+        new_field.append(new_line)
+    return new_field
+
+
 def try_move(field, pos, delta):
     i, j = pos
     ty = field[i][j]
@@ -51,8 +72,9 @@ def compute_score(field):
     s = 0
     for i in range(len(field)):
         for j in range(len(field[0])):
-            if field[i][j] == "O":
+            if field[i][j] == "O" or field[i][j] == "[":
                 s += 100 * i + j
+
     return s
 
 
@@ -62,6 +84,60 @@ def part1():
 
     for move in moves:
         _, pos = try_move(field, pos, deltas[move])
+
+    print_field(field)
+    print(compute_score(field))
+
+
+def can_move(field, pos, delta, ignore_half=False):
+    i, j = pos
+    ty = field[i][j]
+    di, dj = delta
+    ni, nj = i + di, j + dj
+
+    if ty == "#":
+        return False
+    if ty == ".":
+        return True
+
+    if (ty == "[" or ty == "]") and dj == 0 and not ignore_half:
+        mi, mj = (i, j + 1) if ty == "[" else (i, j - 1)
+        if not can_move(field, (mi, mj), delta, ignore_half=True):
+            return False
+
+    return can_move(field, (ni, nj), delta)
+
+
+def apply_move(field, pos, delta, ignore_half=False):
+    i, j = pos
+    ty = field[i][j]
+    di, dj = delta
+    ni, nj = i + di, j + dj
+
+    if ty == "#":
+        raise RuntimeError("cannot move wall")
+    if ty == ".":
+        return (i, j)
+
+    if (ty == "[" or ty == "]") and dj == 0 and not ignore_half:
+        mi, mj = (i, j + 1) if ty == "[" else (i, j - 1)
+        apply_move(field, (mi, mj), delta, ignore_half=True)
+
+    apply_move(field, (ni, nj), delta)
+    field[i][j] = "."
+    field[ni][nj] = ty
+    return (ni, nj)
+
+
+def part2():
+    field, moves = read_input()
+    field = blowup(field)
+    pos = find_robot(field)
+
+    for move in moves:
+        movable = can_move(field, pos, deltas[move])
+        if movable:
+            pos = apply_move(field, pos, deltas[move])
 
     print_field(field)
     print(compute_score(field))
